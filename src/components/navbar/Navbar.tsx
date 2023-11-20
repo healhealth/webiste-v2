28038f2372a5f8c7c3a43cdf933c72e61c2812a5
Helen <helen.hagos@integrify.io>
2023-11-20 09:24:45 +0100

    added mobile menu and toggle button

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,31 +1,87 @@
+import React, { useRef } from "react";
 import { Link } from "react-router-dom";
 
 import "./Navbar.css";
 import logo from "../../assets/heal-health-logo-192.png";
-
 const Navbar = () => {
+  const toggleButton = useRef<HTMLDivElement>(null);
+  const mobileMenu = useRef<HTMLDivElement>(null);
+
+  const handleOpenMobileMenu = () => {
+    mobileMenu.current?.classList.toggle("active");
+  };
+  const handleCloseMobileMenu = () => {
+    mobileMenu.current?.classList.toggle("active");
+  };
   return (
-    <div className="navbar-wrapper">
-      <div className="logo-container">
-        <img className="logo-img" src={logo} alt="logo" />
-        <p className="logo-text">Heal Health</p>
-      </div>
-      <div className="nav-link-wrapper">
-        <Link className="nav-link" to="/">
-          Home
-        </Link>
-        <Link className="nav-link" to="/about">
-          About
-        </Link>
-        <Link className="nav-link" to="/services">
-          Services
-        </Link>
-        <Link className="nav-link" to="/ourteam">
-          Team
-        </Link>
-        <Link className="nav-link" to="/join">
-          Join us
-        </Link>
+    <div className="site-navbar">
+      <div className="container">
+        <div className="logo">
+          <img className="logo-img" src={logo} alt="logo" />
+          <p className="logo-text">Heal Health</p>
+        </div>
+        <div ref={toggleButton} onClick={handleOpenMobileMenu}>
+          <a href="#" className="toggle-button">
+            <span className="bar"></span>
+            <span className="bar"></span>
+            <span className="bar"></span>
+          </a>
+        </div>
+        {/* Mobile menu */}
+        <div className="mobile-menu" ref={mobileMenu}>
+          <ul>
+            <Link className="nav-link" to="/" onClick={handleCloseMobileMenu}>
+              Home
+            </Link>
+            <Link
+              className="nav-link"
+              to="/about"
+              onClick={handleCloseMobileMenu}
+            >
+              About us
+            </Link>
+            <Link
+              className="nav-link"
+              to="/services"
+              onClick={handleCloseMobileMenu}
+            >
+              Services
+            </Link>
+            <Link
+              className="nav-link"
+              to="/team"
+              onClick={handleCloseMobileMenu}
+            >
+              Team
+            </Link>
+            <Link
+              className="nav-link"
+              to="/join"
+              onClick={handleCloseMobileMenu}
+            >
+              Join us
+            </Link>
+          </ul>
+        </div>
+        <div className="nav-links-container">
+          <ul>
+            <Link className="nav-link" to="/">
+              Home
+            </Link>
+            <Link className="nav-link" to="/about">
+              About us
+            </Link>
+            <Link className="nav-link" to="/services">
+              Services
+            </Link>
+            <Link className="nav-link" to="/ourteam">
+              Team
+            </Link>
+            <Link className="nav-link" to="/join">
+              Join us
+            </Link>
+          </ul>
+        </div>
       </div>
     </div>
   );
