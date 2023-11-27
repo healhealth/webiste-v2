562030f94906453763aacb3ead64c916e13da59e
Helen <helen.hagos@integrify.io>
2023-11-27 13:36:53 +0100

    updated image height

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -5,9 +5,7 @@ import OurVisionAndMission from "../ourVisionAndMission/OurVisionAndMission";
 const Home = () => (
   <div className="home">
     <div className="container">
-      <div className="background-text-container">
-        <p>Democratizing Mental Well-Being</p>
-      </div>
+      <div className="background-text-container"></div>
     </div>
     <div className="container-sm">
       <OurVisionAndMission />
